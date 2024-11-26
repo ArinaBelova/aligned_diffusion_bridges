f6bd0d9e22a20be750f99d1d856d13709de4c619
Arina Belova <arishkabelova1999@gmail.com>
2024-11-26 18:25:19 +0100

    Workng torch packages specifications

--- build_env.sh
+++ build_env.sh
@@ -2,34 +2,38 @@
 
 source $(conda info --root)/etc/profile.d/conda.sh
 
-conda create -y --name sbalign python=3.9
-conda activate sbalign
+conda create -y --name sbalign_test python=3.9
+conda activate sbalign_test
 
 python -m pip install --upgrade pip
 conda env update --file $PWD/env.yml
 
-TORCH=1.12.0
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    CUDA=cu113
-else
-    CUDA=cpu
-fi
+TORCH=2.4.0
+CUDA=cu124
+
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#     CUDA=cu124
+# else
+#     CUDA=cpu
+# fi
 
 ARCH=$(uname -m)
 
-python -m pip install torch_geometric
+python -m pip install --upgrade --force-reinstall torch_geometric
 
-if [[ "$ARCH" == "arm64" ]]; then
-    python -m pip install torch_scatter
-    python -m pip install torch_sparse
-    python -m pip install torch_cluster
-else
-    python -m pip install --no-index \
-        torch_scatter \
-        torch_sparse \
-        torch_cluster \
-        -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-fi
+# if [[ "$ARCH" == "arm64" ]]; then
+#     python -m pip install torch_scatter
+#     python -m pip install torch_sparse
+#     python -m pip install torch_cluster
+# else
+#     python -m pip install --upgrade --force-reinstall --no-index \
+#         torch_scatter \
+#         torch_sparse \
+#         torch_cluster \
+#         -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+# fi
+
+python -m pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
 
 python -m pip install --upgrade e3nn
 python setup.py develop
