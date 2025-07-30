d9cf11500da4dcdde7fe9d8e42b9871a08ef3746
Arina Belova <arishkabelova1999@gmail.com>
2025-07-30 16:39:51 +0200

    Change in evaluation script for holo->apo direction

--- scripts/conf/evaluate.py
+++ scripts/conf/evaluate.py
@@ -9,7 +9,7 @@ import torch
 from torch_geometric.loader import DataLoader
 
 import os
-os.chdir("/home/fe/nobis/projects/bridges/aligned_diffusion_bridges") 
+os.chdir("/home/fe/belova/projects/bridges/aligned_diffusion_bridges") 
 import sys
 sys.path.append(os.getcwd())
 
@@ -37,7 +37,10 @@ def prepare_inference_setup(args):
 
     model_dict = torch.load(f"{args.log_dir}/{args.run_name}/{args.model_name}",
                              map_location='cpu')
-    model.load_state_dict(model_dict)
+    if args.model_name == "last_model.pt":
+        model.load_state_dict(model_dict["model"])
+    else:                                 
+        model.load_state_dict(model_dict)
     model.to(DEVICE)
 
     print('In prepare inference setup',args.data_dir)
@@ -108,18 +111,23 @@ def run_inference_baseline(data, model):
 
 
 def print_statistics(args, metrics):
-    rmsds = []
-
     # RMSD associated statistics
     print(flush=True)
     rmsds = []
+    delta_rmsd  = []
     for pdb_dict in metrics:
         rmsds.extend(pdb_dict['rmsd'])
+        delta_rmsd.extend((np.array(pdb_dict['init_rmsd']) - np.array(pdb_dict['rmsd'])).tolist())
     
     mean_rmsd = np.round(np.mean(rmsds), 4)
     median_rmsd = np.round(np.median(rmsds), 4)
     std_rmsd = np.round(np.std(rmsds), 4)
     print(f"RMSD ({args.n_samples}): Mean={mean_rmsd} | Std={std_rmsd} | Median={median_rmsd}")
+
+    mean_delta_rmsd = np.round(np.mean(delta_rmsd), 4)
+    median_delta_rmsd = np.round(np.median(delta_rmsd), 4)
+    std_delta_rmsd = np.round(np.std(delta_rmsd), 4)
+    print(f"DELTA RMSD (data - model) ({args.n_samples}): Mean={mean_delta_rmsd} | Std={std_delta_rmsd} | Median={median_delta_rmsd}")
     
     rmsd_stats = "RMSD (% <) | "
     for threshold in [2.0, 5.0, 10.0]:
@@ -128,7 +136,25 @@ def print_statistics(args, metrics):
     print(rmsd_stats, flush=True)
     print(flush=True)
 
+    delta_rmsd_stats = "DELTA RMSD (% <) | "
+    for threshold in [2.0, 5.0, 10.0]:
+        delta_rmsd_less_than_threshold = (np.abs(np.asarray(delta_rmsd)) < np.abs(threshold)).mean()
+        delta_rmsd_stats += f"{threshold}= {np.round(delta_rmsd_less_than_threshold, 4)} | "
+    print(delta_rmsd_stats, flush=True)
 
+def str2bool(s):
+    # s is already bool
+    if isinstance(s, bool):
+        return s
+    # s is string repr. of bool
+    if s.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif s.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    # s is something else
+    else:
+        return s
+    
 def parse_args():
 
     parser = argparse.ArgumentParser()
@@ -137,6 +163,7 @@ def parse_args():
     parser.add_argument("--log_dir", type=str)
     parser.add_argument("--run_name", type=str)
     parser.add_argument("--model_name", type=str)
+    parser.add_argument("--inverse_problem", type=str2bool)
 
     parser.add_argument("--method", type=str)
 
@@ -147,8 +174,7 @@ def parse_args():
 
     args = parser.parse_args()
     return args
-
-
+    
 def main():
     args = parse_args()
     
@@ -156,6 +182,10 @@ def main():
 
     metrics = []
     for data in loader:
+        if args.inverse_problem:
+            data_pos_0_copy = data.pos_0.clone()
+            data.pos_0 = data.pos_T
+            data.pos_T = data_pos_0_copy
 
         if "conf_id" in data:
             if len(data['conf_id']) > 0:
